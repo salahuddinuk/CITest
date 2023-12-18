8352d3f5401d8480730e2c4577cf6753af7200f4
Salah <salahuddin.uk@gmail.com>
2023-12-18 13:47:45 +0400

    d

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -22,8 +22,8 @@ namespace WebApplication1.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             int num=Random.Shared.Next(0, 999);
-            int nums = Random.Shared.Next(0, 999);
-            int nums3 = Random.Shared.Next(0, 999);
+            int num2 = Random.Shared.Next(1000, 9999);
+            
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
